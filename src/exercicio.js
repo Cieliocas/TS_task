147ed77f1f42467848a5b001a8fea7b25fd21163
Cieliocas <francielioevangelista@hotmail.com>
2024-12-19 01:57:02 -0300

    ts exercicio

--- src/exercicio.js
+++ src/exercicio.js
@@ -1,7 +1,15 @@
 "use strict";
 // Exercício de TS
-// Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+// Utilizando o TypeScript e a tipagem escreva duas funções:
+// -> uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles,
+// -> e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
 // A função de multiplicação deverá retornar o resultado da operação com a frase "Olá ${nome}", o resultado da multiplicação é:
 function dizOla(nome) {
     return `Olá ${nome}, o resultado da multiplicação é:`;
 }
+// A função de multiplicação deverá retornar o resultado da operação.
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+// Testes
+console.log(dizOla('Lucas'), multiplicacao(2, 3)); // Olá Lucas, o resultado da multiplicação é: 6
--- src/exercicio.ts
+++ src/exercicio.ts
@@ -1,9 +1,18 @@
 // Exercício de TS
 
-// Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+// Utilizando o TypeScript e a tipagem escreva duas funções:
+// -> uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles,
+// -> e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
 
 // A função de multiplicação deverá retornar o resultado da operação com a frase "Olá ${nome}", o resultado da multiplicação é:
 function dizOla(nome: string): string {
     return `Olá ${nome}, o resultado da multiplicação é:`;
 }
 
+// A função de multiplicação deverá retornar o resultado da operação.
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Testes
+console.log(dizOla('Lucas'), multiplicacao(2, 3)); // Olá Lucas, o resultado da multiplicação é: 6
