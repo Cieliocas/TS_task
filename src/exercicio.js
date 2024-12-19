800578cc674f629445057dfdbe3abdbf8e9227ff
Cieliocas <francielioevangelista@hotmail.com>
2024-12-19 01:53:52 -0300

    ts exercicio

--- src/exercicio.js
+++ src/exercicio.js
@@ -1,1 +1,7 @@
 "use strict";
+// Exercício de TS
+// Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+// A função de multiplicação deverá retornar o resultado da operação com a frase "Olá ${nome}", o resultado da multiplicação é:
+function dizOla(nome) {
+    return `Olá ${nome}, o resultado da multiplicação é:`;
+}
---
+++ src/exercicio.ts
@@ -0,0 +1,9 @@
+// Exercício de TS
+
+// Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+
+// A função de multiplicação deverá retornar o resultado da operação com a frase "Olá ${nome}", o resultado da multiplicação é:
+function dizOla(nome: string): string {
+    return `Olá ${nome}, o resultado da multiplicação é:`;
+}
+
